aea6fa4ac90c09e24a69c4af13df04ce194d3da4
MINHEE <MINHEE@MINHEE-LAPTOP>
2022-05-10 23:38:18 +0900

    초기 커밋.

---
+++ MainForm.Designer.cs
@@ -0,0 +1,169 @@
+﻿namespace MiniNote
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// 필수 디자이너 변수입니다.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 사용 중인 모든 리소스를 정리합니다.
+        /// </summary>
+        /// <param name="disposing">관리되는 리소스를 삭제해야 하면 true이고, 그렇지 않으면 false입니다.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 디자이너에서 생성한 코드
+
+        /// <summary>
+        /// 디자이너 지원에 필요한 메서드입니다. 
+        /// 이 메서드의 내용을 코드 편집기로 수정하지 마세요.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MainForm));
+            this.introPanel = new System.Windows.Forms.TableLayoutPanel();
+            this.startButton = new System.Windows.Forms.Button();
+            this.logoBox = new System.Windows.Forms.PictureBox();
+            this.blLabel = new System.Windows.Forms.Label();
+            this.mainMenu = new System.Windows.Forms.TabControl();
+            this.tabPage1 = new System.Windows.Forms.TabPage();
+            this.tabPage2 = new System.Windows.Forms.TabPage();
+            this.introPanel.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.logoBox)).BeginInit();
+            this.mainMenu.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // introPanel
+            // 
+            this.introPanel.BackColor = System.Drawing.SystemColors.ControlLightLight;
+            this.introPanel.ColumnCount = 1;
+            this.introPanel.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.introPanel.Controls.Add(this.startButton, 0, 3);
+            this.introPanel.Controls.Add(this.logoBox, 0, 1);
+            this.introPanel.Controls.Add(this.blLabel, 0, 2);
+            this.introPanel.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.introPanel.Location = new System.Drawing.Point(0, 0);
+            this.introPanel.Name = "introPanel";
+            this.introPanel.RowCount = 5;
+            this.introPanel.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 85F));
+            this.introPanel.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 160F));
+            this.introPanel.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 68F));
+            this.introPanel.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 100F));
+            this.introPanel.RowStyles.Add(new System.Windows.Forms.RowStyle());
+            this.introPanel.Size = new System.Drawing.Size(384, 461);
+            this.introPanel.TabIndex = 0;
+            // 
+            // startButton
+            // 
+            this.startButton.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.startButton.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.startButton.FlatAppearance.BorderColor = System.Drawing.SystemColors.ControlLight;
+            this.startButton.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.startButton.Location = new System.Drawing.Point(142, 346);
+            this.startButton.Name = "startButton";
+            this.startButton.Size = new System.Drawing.Size(100, 34);
+            this.startButton.TabIndex = 2;
+            this.startButton.Text = "시작하기";
+            this.startButton.UseVisualStyleBackColor = true;
+            this.startButton.Click += new System.EventHandler(this.startButton_Click);
+            // 
+            // logoBox
+            // 
+            this.logoBox.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.logoBox.Image = ((System.Drawing.Image)(resources.GetObject("logoBox.Image")));
+            this.logoBox.Location = new System.Drawing.Point(3, 88);
+            this.logoBox.Name = "logoBox";
+            this.logoBox.Size = new System.Drawing.Size(378, 154);
+            this.logoBox.SizeMode = System.Windows.Forms.PictureBoxSizeMode.CenterImage;
+            this.logoBox.TabIndex = 0;
+            this.logoBox.TabStop = false;
+            // 
+            // blLabel
+            // 
+            this.blLabel.AutoSize = true;
+            this.blLabel.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.blLabel.Font = new System.Drawing.Font("Noto Mono for Powerline", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.blLabel.ForeColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.blLabel.Location = new System.Drawing.Point(3, 245);
+            this.blLabel.Name = "blLabel";
+            this.blLabel.Size = new System.Drawing.Size(378, 68);
+            this.blLabel.TabIndex = 1;
+            this.blLabel.Text = "MINI\r\nNOTE";
+            this.blLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // mainMenu
+            // 
+            this.mainMenu.Controls.Add(this.tabPage1);
+            this.mainMenu.Controls.Add(this.tabPage2);
+            this.mainMenu.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.mainMenu.Location = new System.Drawing.Point(0, 0);
+            this.mainMenu.Name = "mainMenu";
+            this.mainMenu.SelectedIndex = 0;
+            this.mainMenu.Size = new System.Drawing.Size(384, 461);
+            this.mainMenu.SizeMode = System.Windows.Forms.TabSizeMode.Fixed;
+            this.mainMenu.TabIndex = 1;
+            this.mainMenu.Visible = false;
+            // 
+            // tabPage1
+            // 
+            this.tabPage1.Location = new System.Drawing.Point(4, 26);
+            this.tabPage1.Name = "tabPage1";
+            this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage1.Size = new System.Drawing.Size(376, 431);
+            this.tabPage1.TabIndex = 0;
+            this.tabPage1.Text = "tabPage1";
+            this.tabPage1.UseVisualStyleBackColor = true;
+            // 
+            // tabPage2
+            // 
+            this.tabPage2.Location = new System.Drawing.Point(4, 26);
+            this.tabPage2.Name = "tabPage2";
+            this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage2.Size = new System.Drawing.Size(376, 431);
+            this.tabPage2.TabIndex = 1;
+            this.tabPage2.Text = "tabPage2";
+            this.tabPage2.UseVisualStyleBackColor = true;
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 17F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(384, 461);
+            this.Controls.Add(this.mainMenu);
+            this.Controls.Add(this.introPanel);
+            this.Font = new System.Drawing.Font("맑은 고딕", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "MainForm";
+            this.Text = "MININOTE";
+            this.introPanel.ResumeLayout(false);
+            this.introPanel.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.logoBox)).EndInit();
+            this.mainMenu.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TableLayoutPanel introPanel;
+        private System.Windows.Forms.PictureBox logoBox;
+        private System.Windows.Forms.Label blLabel;
+        private System.Windows.Forms.Button startButton;
+        private System.Windows.Forms.TabControl mainMenu;
+        private System.Windows.Forms.TabPage tabPage1;
+        private System.Windows.Forms.TabPage tabPage2;
+    }
+}
+
---
+++ MainForm.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MiniNote
+{
+    public partial class MainForm : Form
+    {
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+        private void startButton_Click(object sender, EventArgs e)
+        {
+            introPanel.Visible = false;
+            mainMenu.Visible = true;
+        }
+    }
+}
